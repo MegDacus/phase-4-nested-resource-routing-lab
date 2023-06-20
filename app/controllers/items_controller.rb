080dba45df613a832f6364da2df2ad0b6e054f05
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-06-20 16:20:20 +1000

    Added item #show, #index, #create methods to user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
+ rescue_from ActiveRecord::RecordNotFound, with: :not_found_response 
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else  
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show 
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(params.permit(:name, :description, :price, :user_id))
+    render json: item, status: :created
+  end
+
+  private
+
+  def not_found_response
+    render json: {errors: "Review not found"}, status: :not_found
   end
 
 end
